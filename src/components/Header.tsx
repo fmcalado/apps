1da68b4ef4f8b944071f3543a9a47741277bca26
Calado <wishsolidor@gmail.com>
2022-06-19 17:57:36 -0300

    feat: add image to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import Link from 'next/link';
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import { BiSearchAlt } from 'react-icons/bi';
 import { BsPlusLg } from 'react-icons/bs';
 
@@ -9,17 +10,17 @@ const Header: React.FC = () => {
       {/* Home & Search Buttons */}
       <div className="w-1/3 flex">
         <Link href="/search">
-          <a className="btn-pale block md:hidden p-0 sm:w-12 sm:h-12 w-9 h-9 text-white">
+          <a className="btn-pale block sm:hidden p-0 w-12 h-12 text-white">
             <div className="flex w-full h-full items-center justify-center">
-              <BiSearchAlt className='w-5 h-5 sm:w-6 sm:h-6'/>
+              <BiSearchAlt className="w-6 h-6" />
             </div>
           </a>
         </Link>
         <Link href="/search">
-          <a className="btn-pale hidden md:block w-28 h-12 text-white font-semibold">
+          <a className="btn-pale hidden sm:block w-28 h-12 text-white font-semibold">
             <div className="flex w-full h-full items-center justify-center">
-              <div className='mr-1'>
-                <BiSearchAlt className='w-5 h-5'/>
+              <div className="mr-1">
+                <BiSearchAlt className="w-5 h-5" />
               </div>
               Pesquisar
             </div>
@@ -27,7 +28,28 @@ const Header: React.FC = () => {
         </Link>
       </div>
       {/* Logo */}
-      <div className="w-1/3 flex justify-center">LOGO</div>
+      <div className="w-1/3 flex justify-center">
+        <Link href="/">
+          <a className="block md:hidden">
+            <Image
+              src={'/calado.png'}
+              alt="Calado Logo"
+              width={64}
+              height={64}
+            />
+          </a>
+        </Link>
+        <Link href="/">
+          <a className="hidden md:block">
+            <Image
+              src={'/caladoLogo1.png'}
+              alt="Calado Logo"
+              width={192}
+              height={48}
+            />
+          </a>
+        </Link>
+      </div>
       {/* Submit App Button */}
       <div className="w-1/3 flex justify-end">
         <Link href="/login">
@@ -41,7 +63,7 @@ const Header: React.FC = () => {
           </a>
         </Link>
         <Link href="/login">
-          <a className="btn block sm:hidden text-white font-semibold p-0 w-14 h-10">
+          <a className="btn block sm:hidden text-white font-semibold p-0 w-12 h-12">
             <div className="inline-flex justify-center items-center w-full h-full">
               <BsPlusLg />
             </div>
