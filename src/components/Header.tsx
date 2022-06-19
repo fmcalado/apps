4d771aefe831675148e6a4fafe432cde566c0c55
Calado <wishsolidor@gmail.com>
2022-06-19 14:54:24 -0300

    refactor: change header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiOutlineHome } from 'react-icons/ai';
 import { BiSearchAlt } from 'react-icons/bi';
 import { BsPlusLg } from 'react-icons/bs';
 
@@ -9,17 +8,20 @@ const Header: React.FC = () => {
     <div className="w-full flex items-center justify-between">
       {/* Home & Search Buttons */}
       <div className="w-1/3 flex">
-        <Link href="/">
-          <a className="btn-pale inline-flex items-center justify-center p-0 w-9 h-9 md:w-12 md:h-12 text-white mr-2">
-            <div>
-              <AiOutlineHome size={20} />
+        <Link href="/search">
+          <a className="btn-pale block md:hidden p-0 sm:w-12 sm:h-12 w-9 h-9 text-white">
+            <div className="flex w-full h-full items-center justify-center">
+              <BiSearchAlt className='w-5 h-5 sm:w-6 sm:h-6'/>
             </div>
           </a>
         </Link>
         <Link href="/search">
-          <a className="btn-pale inline-flex items-center justify-center p-0 w-9 h-9 md:w-12 md:h-12 text-white">
-            <div>
-              <BiSearchAlt size={20} />
+          <a className="btn-pale hidden md:block w-28 h-12 text-white font-semibold">
+            <div className="flex w-full h-full items-center justify-center">
+              <div className='mr-1'>
+                <BiSearchAlt className='w-5 h-5'/>
+              </div>
+              Pesquisar
             </div>
           </a>
         </Link>
