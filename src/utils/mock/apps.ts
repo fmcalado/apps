dd84262e083109c5fa56f82af61180e33cf09670
Calado <wishsolidor@gmail.com>
2022-06-20 00:06:11 -0300

    feat: mock data for tests

---
+++ src/utils/mock/apps.ts
@@ -0,0 +1,29 @@
+import { lipsum } from '..';
+import { AppData } from '../../lib/types/AppData';
+
+export const mockApps: AppData[] = [
+  {
+    id: 'asdasdasd',
+    title: 'Arcanth Shop',
+    description: lipsum,
+    thumbnailUrl: 'https://arcanth.net/image-posts/62155851.png',
+    tags: ['Flutter', 'Supabase'],
+    views: 30,
+  },
+  {
+    id: 'asdasdasdasdasd',
+    title: 'Arcanth Shop',
+    description: lipsum,
+    thumbnailUrl: 'https://arcanth.net/image-posts/62155851.png',
+    tags: ['Flutter', 'Supabase'],
+    views: 30,
+  },
+  {
+    id: 'asdasdassssssd',
+    title: 'Arcanth Shop',
+    description: lipsum,
+    thumbnailUrl: 'https://arcanth.net/image-posts/62155851.png',
+    tags: ['Flutter', 'Supabase'],
+    views: 30,
+  },
+];
