59fed697e6e4dfed0e0a253b956d16e015a785d9
Calado <wishsolidor@gmail.com>
2022-06-20 00:04:15 -0300

    refactor: remove logo temporarily

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import Image from 'next/image';
+// import Image from 'next/image';
 import Link from 'next/link';
 import { BiSearchAlt } from 'react-icons/bi';
 import { BsPlusLg } from 'react-icons/bs';
 
-import caladoShortLogo from '../assets/calado.png';
-import caladoFullLogo from '../assets/caladoLogo.png';
+// import caladoShortLogo from '../assets/calado.png';
+// import caladoFullLogo from '../assets/caladoLogo.png';
 
 const ResponsiveSearchButton = () => (
   <div className="w-1/3 flex">
     <Link href="/search">
-      <a className="btn-pale block sm:hidden p-0 w-12 h-12 text-white">
+      <a className="btn-pale block sm:hidden p-0 w-12 h-12">
         <div className="flex w-full h-full items-center justify-center">
           <BiSearchAlt className="w-6 h-6" />
         </div>
       </a>
     </Link>
     <Link href="/search">
-      <a className="btn-pale hidden sm:block w-28 h-12 text-white font-semibold">
+      <a className="btn-pale hidden sm:block w-28 h-12 font-semibold">
         <div className="flex w-full h-full items-center justify-center">
           <div className="mr-1">
             <BiSearchAlt className="w-5 h-5" />
@@ -30,37 +30,39 @@ const ResponsiveSearchButton = () => (
 );
 
 const ResponsiveLogo = () => (
-  <div className="w-1/3 flex justify-center">
-    <Link href="/">
-      <a className="block md:hidden">
-        <Image
-          src={caladoShortLogo}
-          alt="Calado's Short Logo"
-          width={64}
-          height={64}
-        />
-      </a>
-    </Link>
-    <Link href="/">
-      <a className="hidden md:block relative">
-        <Image
-          src={caladoFullLogo}
-          alt="Calado's Full Logo"
-          width={192}
-          height={48}
-          layout="raw"
-        />
-      </a>
-    </Link>
-  </div>
+  <div className='w-1/3' />
+
+  // <div className="w-1/3 flex justify-center">
+  //   <Link href="/">
+  //     <a className="block md:hidden">
+  //       <Image
+  //         src={caladoShortLogo}
+  //         alt="Calado's Short Logo"
+  //         width={64}
+  //         height={64}
+  //       />
+  //     </a>
+  //   </Link>
+  //   <Link href="/">
+  //     <a className="hidden md:block relative">
+  //       <Image
+  //         src={caladoFullLogo}
+  //         alt="Calado's Full Logo"
+  //         width={192}
+  //         height={48}
+  //         layout="raw"
+  //       />
+  //     </a>
+  //   </Link>
+  // </div>
 );
 
 const ResponsiveSubmitButton = () => (
   <div className="w-1/3 flex justify-end">
     <Link href="/login">
-      <a className="btn text-white font-semibold hidden sm:block">
-        <div className="inline-flex items-center">
-          <div className="mr-2">
+      <a className="btn font-semibold hidden sm:block">
+        <div className="flex items-center justify-center">
+          <div className="mr-2 ">
             <BsPlusLg />
           </div>
           Envie seu app
@@ -68,8 +70,8 @@ const ResponsiveSubmitButton = () => (
       </a>
     </Link>
     <Link href="/login">
-      <a className="btn block sm:hidden text-white font-semibold p-0 w-12 h-12">
-        <div className="inline-flex justify-center items-center w-full h-full">
+      <a className="btn block sm:hidden font-semibold p-0 w-12 h-12">
+        <div className="flex items-center justify-center w-full h-full">
           <BsPlusLg />
         </div>
       </a>
