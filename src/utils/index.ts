a525036a372ef517b518564fcc0a98a77ca327fd
Calado <wishsolidor@gmail.com>
2022-06-22 16:31:17 -0300

    feat: give more mock data

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,6 @@
 export const lipsum =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in aliquet dui. In rhoncus egestas enim, tempor laoreet quam tincidunt sed. Aenean hendrerit lacus mi, sed porta nulla accumsan nec. Vestibulum faucibus urna id nibh eleifend, non molestie ante ultrices. Quisque consectetur fermentum quam. Ut vel ligula egestas, ullamcorper massa vitae, porttitor eros. Sed vestibulum non justo in semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras ipsum ligula, maximus vel laoreet sed, gravida sed erat. Praesent sit amet justo lectus.';
+
+export function classNames(...classes: any) {
+  return classes.filter(Boolean).join(' ');
+}
